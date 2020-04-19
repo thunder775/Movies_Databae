6702f7c841c5f000fadc2a7a5a61ac749bf9eedf
thunder775 <rahulmasih61@gmail.com>
2020-04-19 13:51:21 +0530

    added search functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Grid from './Grid'
 import './App.css';
 import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
 import DetailsPage from "./details_page";
+// import SearchPage from "./search_page";
 
 function App() {
     return (
         <Router>
             <Switch>
+                {/*<Route path={"/search"} component={SearchPage}/>*/}
                 <Route path={"/details/:id"} component={DetailsPage}/>
                 <Route path={""}><Grid/></Route>
             </Switch>
