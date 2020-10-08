c4e4c92ada81c2f0a5eb0821786386e1b20a2c6a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-08 04:21:56 +0000

    Deploy thunder775/Movies_Database to github.com/thunder775/Movies_Database.git:gh-pages

---
+++ precache-manifest.ac8bb328a3351d7f81c42d7b54a8340b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed5f366b190b6812ac8e9899d72155b",
+    "url": "/Movies_Database/index.html"
+  },
+  {
+    "revision": "93127ddd04c355a472af",
+    "url": "/Movies_Database/static/css/main.d1acc0b7.chunk.css"
+  },
+  {
+    "revision": "6c0cc7e5ef3ef2d9bba8",
+    "url": "/Movies_Database/static/js/2.a70f8d19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movies_Database/static/js/2.a70f8d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93127ddd04c355a472af",
+    "url": "/Movies_Database/static/js/main.85258854.chunk.js"
+  },
+  {
+    "revision": "a30acef4ea11805b329c",
+    "url": "/Movies_Database/static/js/runtime-main.e716001c.js"
+  },
+  {
+    "revision": "3bfccc6c476153ea92ea12ad730bdddd",
+    "url": "/Movies_Database/static/media/logo.3bfccc6c.PNG"
+  }
+]);
